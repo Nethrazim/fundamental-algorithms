74dc12bdcb082cd0a7f21fbccc4394efe85249aa
Sugar Daddy <vlad.branzei@gmail.com>
2025-02-05 06:22:30 +0200

    + Trees

---
+++ FundamentalAlgorithms/FundamentalAlgorithms/Trees/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalAlgorithms.Trees
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node() {
+
+        }
+    }
+
+
+    public static class Samples 
+    {
+        public static Node populateNode(int[] values) { 
+            
+        }
+    }
+}
